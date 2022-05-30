8de9740a13ebda918f95347f765a0755afff109e
Cosmin2410 <cosminnae00@yahoo.com>
2022-05-31 00:12:50 +0300

    logo added and created object for main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 function App() {
-  return <div></div>;
+  const me = {
+    name: 'Cosmin Nae',
+    age: new Date().getFullYear() - 2000,
+    learned: ['HTML', 'CSS', 'JavaScript', 'React'],
+    passionate: true,
+    hobbies: ['Gaming', 'Technology', 'Programming'],
+  };
+
+  return (
+    <div>
+      <img src="images/logo.svg" />
+      <p>{me.name}</p>
+      <p>{me.age}</p>
+      <p>{me.learned}</p>
+      <p>{me.passionate}</p>
+      <p>{me.hobbies}</p>
+    </div>
+  );
 }
 
 export default App;
