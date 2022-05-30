0bbc201d00462e0c45f1995c26f0cbf4afd97280
Cosmin2410 <cosminnae00@yahoo.com>
2022-05-31 00:51:36 +0300

    fonts, colors and sass added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,23 @@ function App() {
     hobbies: ['Gaming', 'Technology', 'Programming'],
   };
 
+  // 'La Belle Aurore',cursive pt <h1> before si after
+  // League Spartan,Helvetica,Arial,sans-serif; pentru heading, titluri
+  // LibreBaskerville-Regular,Palatino Linotype,Times New Roman,serif ---- italic
+
+  // Main bg-color: #1c1d25
+  // Secondary bg-color: #232323
+  // Third color:#5c81e8
+
+  // Gradient cu Third Color si alta putin mai inchisa
+
+  // Fourth-color (contrast):#ef3937
+  // Gradient cu #f06348
+
+  // White: #ffffff
+
+  //npm install --save-dev sass
+
   return (
     <div>
       <img src="images/logo.svg" />
